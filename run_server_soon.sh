35da8846200bf44cfa8271b847111cfd97ff6ebf
pycorn0729 <hayesdominique0729@gmail.com>
2026-04-13 16:59:36 +0000

    refactor: remove run_server.sh and enhance input validation in index.html
    - Deleted the `run_server.sh` script as it is no longer needed.
    - Improved input validation in the staking and moving stake functions by ensuring hotkey and netuid are provided and valid.
    - Refactored JSON payload construction to use trimmed values directly, enhancing code clarity and reducing redundancy.

---
+++ run_server_soon.sh
@@ -0,0 +1,2 @@
+export REAL_ACCOUNT_SS58="5CsiGTsNBAn1bNiGNEd5LYpo6bm3PXT5ogPrQmvpZaUb2XzZ"
+uvicorn app.main:app --host "0.0.0.0" --port 10294
---
+++ run_server_soon_2.sh
@@ -0,0 +1,2 @@
+export REAL_ACCOUNT_SS58="5HCT4AarReToT1BKyLtJXJfSLs4zRS7dENnZ7iysqrqxXyV7"
+uvicorn app.main:app --host "0.0.0.0" --port 10293
