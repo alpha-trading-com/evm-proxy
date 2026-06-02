0685bd8c46448eecd6931045782f34fd1a5d5d07
pycorn0729 <hayesdominique0729@gmail.com>
2026-06-02 12:52:56 +0000

    refactor: update hook_constants.py for improved configuration and clarity
    - Modified the NETWORK constant to ensure proper connection settings.
    - Increased STAKE_AMOUNT_TAO for better clarity on staking requirements.
    - Expanded WHITELISTED_SUBNETS and BLACK_LISTED_COLDKEYS for enhanced network management.
    - Adjusted UNSTAKE_TO_ROOT_IF_PRICE_ABOVE to include additional subnet price thresholds, improving stake management responsiveness.

--- hook/hook_constants.py
+++ hook/hook_constants.py
@@ -1,17 +1,28 @@
 from app.constants import NETWORK
 
-
+NETWORK = "ws://127.0.0.1:9944"
 SEEN_MAX = 1500
 EXTRINSIC_START_CALL = 'START_CALL'
 EXTRINSIC_SUBMIT_ENCRYPTED = 'SUBMIT_ENCRYPTED'
-WHITELISTED_SUBNETS = [40, 58]
-BLACK_LISTED_COLDKEYS = ["5F9Qvcz22Fwq4cm58o2bShiL6n8BnJmhqXB1cispBpqRfN6w"]
-STAKE_AMOUNT_TAO = 1
 ROOT_NETUID = 0
+
+
+STAKE_AMOUNT_TAO = 200
 MIN_STAKE_RAO = 1000_000_001
-NETWORK = "ws://127.0.0.1:9944"
 # netuid -> TAO per alpha; move all stake to root when price is above this
+WHITELISTED_SUBNETS = [16, 40, 58, 92, 72]
+BLACK_LISTED_COLDKEYS = [
+    "5CqRkhQUEgkQ4nBB4SCKnc9AzKPs9VLYv28erjeXPqQYVt9V", 
+    "5F9Qvcz22Fwq4cm58o2bShiL6n8BnJmhqXB1cispBpqRfN6w",
+    "5CigXk8XsnSqi8unxvYma6n8wYD35obs1XCS9eibjFF4vYEN",
+    "5DPhE2hhn6Bbn8QzrFMBTdtZR1QD9NEZDjRv256ppvpJNW92",
+    "5HTYVBxrF2WbVN8RBtFxAkBGuHJxjgLd9Sze5gxH4KC6GLCv",
+]
+
 UNSTAKE_TO_ROOT_IF_PRICE_ABOVE: dict[int, float] = {
-    4: 0.02,
-#    58: 0.02,
+    16: 0.02,
+    40: 0.02,
+    58: 0.02,
+    92: 0.02,
+    72: 0.02,
 }
