616b31ad4dc70fe43763630804294208154bf0c2
pycorn0729 <hayesdominique0729@gmail.com>
2026-06-02 00:36:58 +0000

    refactor: streamline unstake.py by removing unused function and improving clarity
    - Removed the `move_stake_to_root_if_price` function from `unstake.py` to eliminate redundancy and enhance code clarity.
    - This change contributes to a more maintainable and efficient script for stake management.

---
+++ hook/dynamic_unstake.py
@@ -0,0 +1,27 @@
+import bittensor as bt
+import time
+from hook_constants import UNSTAKE_TO_ROOT_IF_PRICE_ABOVE, MIN_STAKE_RAO
+from act import move_stake_to_root, move_stake_to_root_if_price
+from app.core.config import settings
+from app.services.stake_service import get_stake_custom
+
+
+
+if __name__ == "__main__":
+    subtensor = bt.Subtensor("finney")
+    subtensor.wait_for_block()
+
+    while True:
+        current_block = subtensor.get_current_block()
+        subnet_infos = subtensor.all_subnets()
+        coldkey_ss58 = settings.REAL_ACCOUNT_SS58
+        hotkey = settings.DEFAULT_DEST_HOTKEY
+
+        for netuid, ref_price in UNSTAKE_TO_ROOT_IF_PRICE_ABOVE.items():
+            stake_balance = get_stake_custom(subtensor, coldkey_ss58, hotkey, netuid)
+            if stake_balance.rao < MIN_STAKE_RAO:
+                print(f"stake_balance is below MIN_STAKE_RAO for subnet {netuid}")
+                continue
+            
+            move_stake_to_root_if_price(origin_netuid=netuid, ref_price_tao_per_alpha=ref_price, require_above=True, amount_tao=float(stake_balance.tao - 1))
+
